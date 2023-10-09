a321d02600442b5fd9fde7c4a2eede24c1d5bf45
loscarpab <ccormor392@g.educaand.es>
2023-10-09 16:58:49 +0200

    Estructura de clases

--- poo/Program.cs
+++ poo/Program.cs
@@ -1,6 +1,63 @@
 ﻿interface IGrafico
 {
-    public void mover();
-    public int dibujar();
+    public Boolean Mover(int x, int y);
+    public void Dibujar();
+}
 
+public class Punto : IGrafico
+{
+    public int X {  get; private set; }
+    public int Y{ get; private set; }
+    
+    public Punto(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public bool Mover(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dibujar()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Circulo : Punto
+{
+    public int Radio {  get; private set; }
+    public Circulo(int x, int y, int radio) : base(x, y)
+    {
+        Radio = radio;
+    }
+}
+
+public class Rectangulo : Punto
+{
+    public int Ancho { get; private set; }
+    public int Alto { get; private set; }
+    public Rectangulo(int x, int y, int ancho, int alto) : base(x, y)
+    {
+        Ancho = ancho;
+        Alto = alto;
+    }
+}
+
+public class GraficoCompuesto : IGrafico
+{
+    private List<IGrafico> Graficos = new List<IGrafico>();
+
+
+    public void Dibujar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Mover(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
 }
