74e4afe5a1edf9960be0847f0f675ebfffa25678
loscarpab <ccormor392@g.educaand.es>
2023-10-16 17:18:50 +0200

    .

--- poo/Program.cs
+++ poo/Program.cs
@@ -1,31 +1,54 @@
-﻿using System.Transactions;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Transactions;
 
-interface IGrafico
+    EditorGrafico editorGrafico = new EditorGrafico();
+    Punto punto = new Punto(870, 550);
+    editorGrafico.AñadirGrafico(punto);
+    Circulo circulo = new Circulo(50, 50, 2);
+    editorGrafico.AñadirGrafico(circulo);
+    Rectangulo rectangulo = new Rectangulo(100, 100, 5, 5);
+    editorGrafico.AñadirGrafico(rectangulo);
+    Console.WriteLine(editorGrafico.Dibujar());
+
+
+public interface IGrafico
 {
     public Boolean Mover(int x, int y);
-    public void Dibujar();
+    public string Dibujar();
 }
 
 public class Punto : IGrafico
 {
-    public int X {  get; private set; }
-    public int Y{ get; private set; }
+    public int X {  get; protected set; }
+    public int Y{ get; protected set; }
     
     public Punto(int x, int y)
     {
-        X = x;
-        Y = y;
+        try {
+            if (x < 0 || x > 800) { throw new ArgumentOutOfRangeException("El ancho (x) no corresponde con el permitido"); }
+            if (y < 0 || y > 800) { throw new ArgumentOutOfRangeException("El alto (y) no corresponde con el permitido"); }
+            X = x;
+            Y = y;
+        }catch (ArgumentOutOfRangeException ex) 
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 
-    public bool Mover(int x, int y)
+    public virtual bool Mover(int x, int y)
     {
-        if  ((0 > (x + X) && (x + X) > 800) && (0 > (y + Y) && (y + Y) > 600)) return true;
+        if ((0 > (x + X) && (x + X) > 800) && (0 > (y + Y) && (y + Y) > 600)) {
+            X += x;
+            Y += y;
+            return true; 
+        }
         else return false;
     }
 
-    public void Dibujar()
+    public virtual string Dibujar()
     {
-        Console.WriteLine("X -> "+ X + "Y  -> " + Y);
+        return ("X -> "+ X + " Y  -> " + Y);
     }
 }
 
@@ -35,19 +58,24 @@ public class Circulo : Punto
     public Circulo(int x, int y, int radio) : base(x, y)
     {
         Radio = radio;
+        
     }
-    public new bool Mover (int x, int y)
+    public override bool Mover (int x, int y)
     {
         if (!base.Mover(x, y)) return false;
         else if ((0 > (y + Y + Radio) || (y + Y + Radio) > 600)) return false;
-        else if (0 > (x + X + Radio) || (x + X + Radio ) > 800) return false;
+        else if (0 > (x + X + Radio) || (x + X + Radio) > 800) return false;
         else if ((0 > (y + Y - Radio) || (y + Y - Radio) > 600)) return false;
         else if (0 > (x + X - Radio) || (x + X - Radio) > 800) return false;
-        else return true;
+        else {
+            X += x;
+            Y += y;
+            return true;
+        }
     }
-    public new void Dibujar() { 
-        base.Dibujar();
-        Console.WriteLine("Radio -> "+Radio);
+    public override string Dibujar() { 
+        return base.Dibujar() + " Radio -> " + Radio;
+        
     }
 }
 
@@ -57,20 +85,32 @@ public class Rectangulo : Punto
     public int Alto { get; private set; }
     public Rectangulo(int x, int y, int ancho, int alto) : base(x, y)
     {
-        Ancho = ancho;
-        Alto = alto;
+        try
+        {
+            if(x + ancho > 800) { throw new ArgumentOutOfRangeException("El ancho (x) no corresponde con el permitido "); }
+            if (y + alto > 600) { throw new ArgumentOutOfRangeException("El alto (y) no corresponde con el permitido"); }
+            Ancho = ancho;
+            Alto = alto;
+        }catch(ArgumentOutOfRangeException ex) {
+            Console.WriteLine(ex.Message);
+        }
+        
     }
-    public new bool Mover(int x, int y)
+    public override bool Mover(int x, int y)
     {
-       if(!base.Mover(x, y)) return false;
-       else if((0 < (y + Y + Alto) || (y + Y + Alto) > 600)) return true;
-       else if (0 < (x + X + Ancho) || (x + X + Ancho) > 800) return true;
-       else return false;
+        if (!base.Mover(x, y)) return false;
+        else if ((0 > (y + Y + Alto) || (y + Y + Alto) > 600)) return false;
+        else if (0 > (x + X + Ancho) || (x + X + Ancho) > 800) return false;
+        else
+        {
+            X += x;
+            Y += y;
+            return true;
+        }
     }
-    public new void Dibujar()
+    public override string Dibujar()
     {
-        base.Dibujar();
-        Console.WriteLine("Ancho -> " + Ancho + "Alto -> " + Alto);
+        return base.Dibujar() + " Ancho -> " + Ancho + " Alto -> " + Alto;
     }
 }
 
@@ -79,12 +119,19 @@ public class GraficoCompuesto : IGrafico
     private List<IGrafico> Elementos = new List<IGrafico>();
 
 
-    public void Dibujar()
+    public void AñadirGrafico(IGrafico grafico)
     {
+        Elementos.Add(grafico);
+    }
+
+    public string Dibujar()
+    {
+        string str = "";
         foreach (var item in Elementos)
         {
-            item.Dibujar(); 
+            str += item.Dibujar()+"\n"; 
         }
+        return str;
     }
 
     public bool Mover(int x, int y)
@@ -95,4 +142,18 @@ public class GraficoCompuesto : IGrafico
         }
         return true;
     }
+}
+
+public class EditorGrafico
+{
+    private GraficoCompuesto Grafico = new GraficoCompuesto();
+    private int Alto = 600;
+    private int Ancho = 800;
+
+    public void AñadirGrafico(IGrafico grafico) => Grafico.AñadirGrafico(grafico);
+    public string Dibujar()
+    {
+      return Grafico.Dibujar();
+    }
+
 }
